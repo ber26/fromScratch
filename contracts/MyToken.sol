e2005dcfed8bdf15c3c23d326f6a29f217ce807d
ber26 <97327309+ber26@users.noreply.github.com>
2022-01-25 04:40:41 +0200

    Update MyToken.sol
    totalSupply function

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,19 +2,19 @@ pragma solidity ^0.8.0;
 
 interface IERC20 {
 
-    function totalSupply() external view returns (uint)
-    function balanceOf(address _owner) public view returns (uint256 balance)
-    function transfer(address _to, uint256 _value) public returns (bool success)
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success)
-    function approve(address _spender, uint256 _value) public returns (bool success)
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining)
+    function totalSupply() external view returns (uint);
+    function balanceOf(address _owner) public view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+    function approve(address _spender, uint256 _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value)
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value)
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
 }
 
-contract Token is IERC20 {
+contract MyToken is IERC20 {
     address private owner;
     
     mapping (address => uint) private balances;
@@ -25,4 +25,9 @@ contract Token is IERC20 {
         totalSupply_ = _totalSupply;
         balances[owner] = _totalSupply;
     }
+
+    function totalSupply() external view returns (uint){
+        return totalSupply_;
+    }
+
 }
