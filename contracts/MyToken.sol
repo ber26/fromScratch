b3b51d6a4ffb98e243d2c3650dc12accfa6f0dad
ber26 <97327309+ber26@users.noreply.github.com>
2022-01-25 10:57:52 +0200

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 interface IERC20 {
@@ -5,9 +6,9 @@ interface IERC20 {
     function totalSupply() external view returns (uint);
     function balanceOf(address _owner) external view returns (uint256 balance);
     function transfer(address _to, uint256 _value) external returns (bool success);
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-    function approve(address _spender, uint256 _value) public returns (bool success);
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+    function approve(address _spender, uint256 _value) external returns (bool success);
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
@@ -42,26 +43,29 @@ contract MyToken is IERC20 {
         balances[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public virtual override returns (bool success){
-        require(allowance(_from,_to) >= _value, 'Allowance amount is too low!');
-        require(balances[_from] >= _value, 'Insufficient Balance!');
+        require(allowance(_from,_to) >= _value, "Allowance amount is too low!");
+        require(balances[_from] >= _value, "Insufficient Balance!");
 
         balances[_from] -= _value;
         balances[_to] += _value;
 
         emit Transfer(_from, _to, _value);
-    };
+        return true;
+    }
 
     function approve(address _spender, uint256 _value) public virtual override returns (bool success){
         allowances[msg.sender][_spender] = _value;
 
         emit Approval(msg.sender, _spender, _value);
-    };
+        return true;
+    }
 
     function allowance(address _owner, address _spender) public view virtual override returns (uint256 remaining){
         return allowances[_owner][_spender];
-    };
+    }
 
 }
